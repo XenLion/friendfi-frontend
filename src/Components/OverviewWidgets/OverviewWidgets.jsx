cd83b4830c04222effa06ba890e3bd8cc125e99f
XenLion <ajene.hallbarrett@gmail.com>
2022-04-06 23:13:37 -0400

    added logic for widget values.

--- src/Components/OverviewWidgets/OverviewWidgets.jsx
+++ src/Components/OverviewWidgets/OverviewWidgets.jsx
@@ -8,21 +8,25 @@ const OverviewWidgets = ({type}) => {
         case "totalValue":
             data = {
                 header:"Total Portfolio Value",
+                value: "$800,000.00",
             };
             break;
         case "totalCost":
             data = {
                 header: "Total Portfolio Cost",
+                value: "$100,000.00",
             };
             break;
         case "totalProfit":
             data = {
                 header: "Total Profit/Loss",
+                value: "$400,000.00",
             };
             break;
         case "bitcoinPrice":
             data = {
                 header: "Current Bitcoin Price",
+                value: "$45,000",
             };
             break
         default:
@@ -32,7 +36,9 @@ const OverviewWidgets = ({type}) => {
     return (
         <article className="overviewWidgets">
             <header className="widgetHeader">{data.header}</header>
-
+            <main className="widgetMain">
+                <p className="widgetValue">{data.value}</p>
+            </main>
         </article>
     )
 }
