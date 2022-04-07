4af6a587c283745f0096a5e10300f3f59d661fea
XenLion <ajene.hallbarrett@gmail.com>
2022-04-07 17:02:01 -0400

    added additional styling to the overview widgets.

--- src/Components/OverviewWidgets/OverviewWidgets.jsx
+++ src/Components/OverviewWidgets/OverviewWidgets.jsx
@@ -7,25 +7,25 @@ const OverviewWidgets = ({type}) => {
     switch (type) {
         case "totalValue":
             data = {
-                header:"Total Portfolio Value",
+                header:"Total Portfolio Value:",
                 value: "$800,000.00",
             };
             break;
         case "totalCost":
             data = {
-                header: "Total Portfolio Cost",
+                header: "Total Portfolio Cost:",
                 value: "$100,000.00",
             };
             break;
         case "totalProfit":
             data = {
-                header: "Total Profit/Loss",
+                header: "Total Profit/Loss:",
                 value: "$400,000.00",
             };
             break;
         case "bitcoinPrice":
             data = {
-                header: "Current Bitcoin Price",
+                header: "Current Bitcoin Price:",
                 value: "$45,000",
             };
             break
@@ -34,12 +34,19 @@ const OverviewWidgets = ({type}) => {
     }
 
     return (
-        <article className="overviewWidgets">
-            <header className="widgetHeader">{data.header}</header>
-            <main className="widgetMain">
-                <p className="widgetValue">{data.value}</p>
-            </main>
-        </article>
+        <section className="overviewWidgets">
+            <article className="widgetArticle">
+                <header className="widgetHeader">{data.header}</header>
+                <main>
+                    <p className="widgetValue">{data.value}</p>
+                </main>
+            </article>
+            <article className="widgetArticle">
+                <main className="widgetValuePercentage">
+                    10.05%
+                </main>
+            </article>
+        </section>
     )
 }
 
